f204099e74211f289609c9fa80c6889382fcb0aa
main-character-coding <yerimin1999@gmail.com>
2024-01-17 23:02:02 +0900

    Revert "feat: MemberService Singleton"
    This reverts commit 2cf4548cccfd6edf4d26d83cd02d260bf3b734c0.

--- src/main/java/com/ssafy/hellospring/service/MemberService.java
+++ src/main/java/com/ssafy/hellospring/service/MemberService.java
@@ -3,11 +3,6 @@ package com.ssafy.hellospring.service;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-
 import com.ssafy.hellospring.domain.Member;
 import com.ssafy.hellospring.repository.MemberRepository;
 import com.ssafy.hellospring.repository.MemoryMemberRepository;
@@ -15,7 +10,6 @@ import com.ssafy.hellospring.repository.MemoryMemberRepository;
 public class MemberService {
 	private final MemberRepository memberRepository;
 
-	@Autowired
 	public MemberService(MemberRepository memberRepository) {
 		this.memberRepository = memberRepository;
 	}
